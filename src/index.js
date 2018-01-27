69d32f09dce8d778a63ecf7d8878fc30c467df01
Heechul Ryu <ryuhcii@gmail.com>
2018-01-27 21:47:48 +0900

    Use HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Entry from './app/entry'
 
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 let element = document.getElementById('content')
 ReactDOM.render(<Router><Entry /></Router>, element)

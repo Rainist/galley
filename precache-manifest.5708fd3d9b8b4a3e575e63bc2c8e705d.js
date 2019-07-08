30591579d1eb1d8a6330ac97757d76f2583ba819
Heechul Ryu <ryuhcii@gmail.com>
2019-07-08 10:44:20 +0900

    Updates

---
+++ precache-manifest.5708fd3d9b8b4a3e575e63bc2c8e705d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28009121ed3b8961f3f947bad52b548",
+    "url": "/galley/index.html"
+  },
+  {
+    "revision": "e247ff2350f5e50590de",
+    "url": "/galley/static/js/2.d86db6e1.chunk.js"
+  },
+  {
+    "revision": "f83f25c5125efb77f131",
+    "url": "/galley/static/js/main.3009958e.chunk.js"
+  },
+  {
+    "revision": "c1aafd7f9ff70d58321f",
+    "url": "/galley/static/js/runtime~main.8a49597b.js"
+  }
+]);
